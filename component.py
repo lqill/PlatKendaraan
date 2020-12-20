76db22590917fc338c5f3dcdb711076c0ee56111
lqill <elqiliqsandy@gmail.com>
2020-12-20 19:40:11 +0700

    Selesai standar

--- component.py
+++ component.py
@@ -1,18 +1,32 @@
 import pygame
+import pygame.freetype
+import pygame.image
 
 
-class Plat:
-    def __init__(self) -> None:
+class Plat(pygame.sprite.Sprite):
+    def __init__(self, Conveyor, Nomor, FONT) -> None:
         super().__init__()
-        self.body = pygame.image.load('sprites/Plat.png').convert()
-        self.nomor = ""
-        self.speed = 1
-        self.pos = self.body.get_rect().move(0, 20)
+        self.image = pygame.image.load('sprites/Plat.png')
+        self.font = FONT
+        self.nomor = Nomor
+        self.teks, self.teksrect = self.font.render(
+            "", pygame.Color("#FFFFFF"))
+        self.speed = 11
+        self.pos = self.image.get_rect().move(-300, 350)
+        self.moving = Conveyor.working
+
+    def switch(self):
+        self.moving = not self.moving
+
+    def setPlat(self):
+        self.teks, self.teksrect = self.font.render(
+            self.nomor, pygame.Color("#FFFFFF"))
 
     def move(self):
-        self.pos = self.pos.move(0, self.speed)
-        if self.pos.right > 600:
-            self.pos.left = 0
+        if self.moving:
+            self.pos = self.pos.move(self.speed, 0)
+            if self.pos.left > 810:
+                self.kill()
 
 
 class Conveyor(pygame.sprite.Sprite):
@@ -20,12 +34,41 @@ class Conveyor(pygame.sprite.Sprite):
         super(Conveyor, self).__init__()
         self.images = [pygame.image.load("sprites/conv"+str(i)+".png")
                        for i in [0, 60, 120, 180]]
-        self.rect = pygame.Rect(, 400, 605, 60)
         self.index = 0
         self.image = self.images[self.index]
+        self.rect = self.image.get_rect().move(-100, 450)
+        self.working = False
+
+    def switch(self):
+        self.working = not self.working
 
     def update(self):
-        self.index += 1
-        if self.index >= len(self.images):
-            self.index = 0
-        self.image = self.images[self.index]
+        if self.working:
+            self.index += 1
+            if self.index >= len(self.images):
+                self.index = 0
+            self.image = self.images[self.index]
+
+
+class Press(pygame.sprite.Sprite):
+    def __init__(self) -> None:
+        super().__init__()
+        self.image = pygame.image.load("sprites/press.png")
+        self.rect = self.image.get_rect()
+        self.pos = self.rect.move(220, -270)
+        self.speed = 10
+        self.working = False
+
+    def switch(self):
+        self.working = not self.working
+
+    def update(self):
+        if self.working:
+            self.pos = self.pos.move(0, self.speed)
+            if self.pos.top < -270:
+                self.speed = -(self.speed)
+                self.working = False
+            elif self.pos.top > -90:
+                self.speed = -(self.speed)
+                return True
+            return False
--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import pygame
+import pygame.freetype
+import pygame.sprite
+import pygame.image
+import pygame.font
+import pygame.time
+import pygame.event
+import pygame.display
 import pygame_gui
-from component import Plat, Conveyor
-
-from pygame_gui.elements import UIButton, UIImage
+from pygame_gui.elements import UIButton
 from pygame_gui.windows import UIFileDialog
+
+from component import Plat, Conveyor, Press
+
+
+from time import process_time as time
 
 
 class Pabrik:
     def __init__(self) -> None:
         super().__init__()
         pygame.init()
-        self.SIZE = WIDTH, HEIGHT = 800, 600
+        self.FONT = pygame.freetype.Font("sprites/UniversCondensed.ttf", 65)
+        self.SIZE = self.WIDTH, self.HEIGHT = 800, 600
         pygame.display.set_caption('Plat Kendaraan')
         self.window_surface = pygame.display.set_mode(self.SIZE)
         self.ui_manager = pygame_gui.UIManager(self.SIZE)
@@ -20,24 +31,67 @@ class Pabrik:
         self.load_button = UIButton(pygame.Rect((20, 20), (100, 50)),
                                     text="PILIH FILE",
                                     manager=self.ui_manager)
+        self.start_button = UIButton(pygame.Rect((20, 90), (100, 50)),
+                                     text="TURN ON",
+                                     manager=self.ui_manager)
+        self.add_button = UIButton(pygame.Rect((20, 150), (100, 50)),
+                                   text="TAMBAH",
+                                   manager=self.ui_manager)
+        self.add_button.disable()
         self.file_dialog = None
         self.file_csv = None
         self.list_plat = []
         self.font = pygame.font.Font("freesansbold.ttf", 32)
-
+        self.time = time()
         self.clock = pygame.time.Clock()
         self.is_running = True
         self.belt = Conveyor()
+        self.press = Press()
         self.group = pygame.sprite.Group(self.belt)
-        # self.plat_baru = Plat()
+        self.hold = (pygame.image.load("sprites/hold.png"),
+                     pygame.image.load("sprites/hold.png").get_rect().move(220, -130))
+        self.plats = []
+        self.sekali = True
+        self.satusatu = True
+        self.print = False
+        # self.plats.append(Plat(self.belt, "BG 1029 AY", self.FONT))
 
     def run(self):
         while self.is_running:
-            time_delta = self.clock.tick(20)/1000.0
+            time_delta = self.clock.tick(10)/1000.0
+            # Otomatis tambah file
+            if self.start_button.text == "TURN OFF" and not self.sekali and self.satusatu:
+                self.plats.append(
+                    Plat(self.belt, self.list_plat.pop(), self.FONT))
+                self.satusatu = False
+
+            for plat in self.plats:
+                if plat.pos.left == 261 and not self.press.working and self.sekali:
+                    self.belt.switch()
+                    for plat2 in self.plats:
+                        plat2.switch()
+                    self.press.switch()
+                    self.satusatu = True
+                    self.sekali = False
+                    break
+                elif plat.pos.left == 261 and self.print and self.press.working:
+                    plat.setPlat()
+                    self.print = False
+                    break
+
+            if not self.press.working and not self.sekali:
+                self.belt.switch()
+                for plat in self.plats:
+                    plat.switch()
+                self.sekali = True
+            # Tombol dan event
             for event in pygame.event.get():
+
+                # QUIT
                 if event.type == pygame.QUIT:
                     self.is_running = False
 
+                # Tombol Pilih File
                 if (event.type == pygame.USEREVENT and
                     event.user_type == pygame_gui.UI_BUTTON_PRESSED and
                         event.ui_element == self.load_button):
@@ -47,20 +101,32 @@ class Pabrik:
                                                     allow_existing_files_only=True)
                     self.load_button.disable()
 
+                # Tombol Start
                 if (event.type == pygame.USEREVENT and
-                        event.user_type == pygame_gui.UI_FILE_DIALOG_PATH_PICKED):
-                    if self.file_csv is not None:
-                        pass
-                    try:
-                        # disini proses csvnyo
-                        pass
-                    except pygame.error:
-                        pass
+                    event.user_type == pygame_gui.UI_BUTTON_PRESSED and
+                        event.ui_element == self.start_button):
+                    self.belt.switch()
+                    for plat in self.plats:
+                        plat.switch()
+                    if self.start_button.text == "TURN ON":
+                        self.start_button.set_text("TURN OFF")
+                    else:
+                        self.start_button.set_text("TURN ON")
 
+                # Tombol add
+                if (event.type == pygame.USEREVENT and
+                    event.user_type == pygame_gui.UI_BUTTON_PRESSED and
+                        event.ui_element == self.add_button):
+                    self.plats.append(
+                        Plat(self.belt, self.list_plat.pop(), self.FONT))
+                    pass
+
+                # Event file di pilih
                 if (event.type == pygame.USEREVENT and
                     event.user_type == pygame_gui.UI_WINDOW_CLOSE and
                         event.ui_element == self.file_dialog):
                     self.load_button.enable()
+                    self.add_button.enable()
                     try:
                         self.file_csv = self.file_dialog.current_file_path.as_posix()
                         if self.file_csv[-3:] == "csv":
@@ -68,25 +134,29 @@ class Pabrik:
                             with open(self.file_csv, 'r') as f:
                                 self.list_plat = [
                                     i for i in f.read().split("\n")]
-                                pass
                         else:
                             pop_up = pygame_gui.windows.UIMessageWindow(pygame.Rect(
-                                200, 300, 200, 300), window_title="Peringatan", html_message="Pilih file dengan format CSV!")
+                                200, 300, 200, 300), manager=self.ui_manager, window_title="Peringatan", html_message="Pilih file dengan format CSV!")
                     except AttributeError:
                         pop_up = pygame_gui.windows.UIMessageWindow(pygame.Rect(
-                            200, 300, 200, 300), window_title="Peringatan", html_message="Pilih filenya dahulu!")
+                            200, 300, 200, 300), manager=self.ui_manager, window_title="Peringatan", html_message="Pilih filenya dahulu!")
                         break
 
                 self.ui_manager.process_events(event)
 
+            # Render Window
             self.ui_manager.update(time_delta)
             self.window_surface.blit(self.background, (0, 0))
-            # self.window_surface.blit(self.background, self.plat_baru.pos, self.plat_baru.pos)
-            # self.plat_baru.move()
-            # self.window_surface.blit(self.plat_baru.body, self.plat_baru.pos)
-            self.ui_manager.draw_ui(self.window_surface)
             self.group.update()
             self.group.draw(self.window_surface)
+            for plat in self.plats:
+                plat.move()
+                self.window_surface.blit(plat.image, plat.pos)
+                self.window_surface.blit(plat.teks, plat.pos.move(13, 30))
+            self.print = self.press.update()
+            self.window_surface.blit(self.press.image, self.press.pos)
+            self.window_surface.blit(self.hold[0], self.hold[1])
+            self.ui_manager.draw_ui(self.window_surface)
             pygame.display.update()
 
 
